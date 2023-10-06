2580745c32768cdd4edb3606300d66e70b26a716
nevgard <fahmifaza6@gmail.com>
2023-10-06 17:51:05 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F2F5F9",
+        secondary: "#FFFFFF",
+        red: "#EC1B09",
+        orange: "#FF9C1B",
+        light: "#1A100380",
+      },
+      fontFamily: {
+        head: ["Poppins", "sans-serif"],
+        body: ["Raleway", "sans-serif"],
+      },
+    },
+  },
+};
